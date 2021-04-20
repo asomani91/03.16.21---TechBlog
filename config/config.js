5574c36ea675b03e74e419725ec0586943841820
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-20 15:01:37 -0400

    creating folders/content for public & views

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize('mydb', 'root', 'password', {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+
+module.exports = sequelize;
