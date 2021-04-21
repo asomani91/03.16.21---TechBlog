9b2d581517ad1da0a32249c55d94d5f84c33d2bf
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-21 16:08:33 -0400

    fixing host, username & password error

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize('user_db', 'b0e2c319f73269', '63daf449', {
-        host: 'mysql://b0e2c319f73269:63daf449@us-cdbr-east-03.cleardb.com/heroku_723b3053db3b507?reconnect=true',
+        host: 'us-cdbr-east-03.cleardb.com',
         dialect: 'mysql',
         port: 3306
     });
