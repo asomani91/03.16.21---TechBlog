39c676ef8ccdc4f9becc8917f900be2239496d23
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-21 16:12:13 -0400

    fixing db name

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize('user_db', 'b0e2c319f73269', '63daf449', {
+    : new Sequelize('heroku_723b3053db3b507', 'b0e2c319f73269', '63daf449', {
         host: 'us-cdbr-east-03.cleardb.com',
         dialect: 'mysql',
         port: 3306
